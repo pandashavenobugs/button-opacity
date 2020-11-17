207e2fff6b8f15e06b9a4ae7edfeb832a986e3c1
berat <cedinckan@gmail.com>
2020-11-17 09:47:12 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  test(){
+    console.log('link hit')
+  }
   title = 'opacity-button';
 }
